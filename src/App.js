657535b365b571bbcc401547c274205aabfe647c
ducviet1696 <ducviet169614@gmail.com>
2019-07-13 12:31:30 +0700

    use react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}       from 'react';
+import {Router, Route}          from "react-router-dom";
+import routes                   from './router';
+import { createBrowserHistory } from "history";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const history = createBrowserHistory();
+class App extends Component {
+
+    render() {
+        return (
+            <Router history={history}>
+                {routes.map((route, index) => {
+                    return <Route key={index} path={route.path} exact={route.exact} component={ props => {
+                        return (
+                            <route.layout {...props}>
+                                <route.component {...props} />
+                            </route.layout>
+                        )
+                    }} />
+                })}
+            </Router>
+        );
+    }
 }
 
 export default App;
